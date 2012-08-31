4a3d90be3627c15fce4ed86022d54107e54f5509
Manuel Resinas <resinas@us.es>
2012-08-31 12:32:46 +0200

    Changes in EntryPoint (name of test files)

--- raci2bpmn/src/raci2bpmn/EntryPoint.java
+++ raci2bpmn/src/raci2bpmn/EntryPoint.java
@@ -12,10 +12,10 @@ public class EntryPoint {
 	public static void main(String[] args) {
 		ModelHandler handler = new ModelHandler();
 		MatrixHandler matrixHandler = new MatrixHandler();
-		RaciMatrix matrix = matrixHandler.loadMatrix("raciMatrix.json");
-		handler.loadModel("ejemplo.bpmn");
+		RaciMatrix matrix = matrixHandler.loadMatrix("completeRaciMatrix.json");
+		handler.loadModel("initialBP.bpmn");
 		handler.transformProcess(matrix);
-		handler.saveModel("result.bpmn");
+		handler.saveModel("resultBP.bpmn");
 
 	}
 
