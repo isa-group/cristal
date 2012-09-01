05038f12001908b845cedd6d48c7cc4faef8f4c2
Manuel Resinas <resinas@us.es>
2012-09-01 17:26:24 +0200

    First commit of cristal-web. It shows a web page with textareas for both bpmn and raci and generates a new bpmn for them both. Besides it also provides an example of BPMN and RACI.

---
+++ cristal-web/src/main/java/es/us/isa/cristal/web/Raci2BpmnResource.java
@@ -0,0 +1,37 @@
+package es.us.isa.cristal.web;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.logging.Logger;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+import raci.MatrixHandler;
+import raci.RaciMatrix;
+import raci2bpmn.ModelHandler;
+
+@Path("/api/raci2bpmn")
+public class Raci2BpmnResource {
+
+	private static final Logger log = Logger.getLogger(Raci2BpmnResource.class
+			.getName());
+
+	@POST 
+	public String raci2Bpmn(@FormParam("raci") String raci, @FormParam("bpmn") String bpmn){
+		ModelHandler handler = new ModelHandler();
+		MatrixHandler matrixHandler = new MatrixHandler();
+
+		log.info("Raci model: "+raci);
+		log.info("bpmn model: "+bpmn);
+		RaciMatrix matrix = matrixHandler.loadMatrix(raci);
+		
+		InputStream is = new ByteArrayInputStream(bpmn.getBytes());
+		handler.loadModel(is);
+		handler.transformProcess(matrix);
+		
+		return handler.getModel();
+	}
+	
+}
